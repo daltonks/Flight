9a49c5d23c6d177e4a80a963977ece7d169861c4
Dalton Spillman <dalton_spillman@yahoo.com>
2016-03-26 11:38:47 -0500

    Initial commit

---
+++ android/src/com/github/daltonks/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.github.daltonks;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.stencil = 8;
+		config.numSamples = 2;
+		initialize(new Game(), config);
+	}
+}
---
+++ core/src/com/github/daltonks/Game.java
@@ -0,0 +1,40 @@
+package com.github.daltonks;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Game extends ApplicationAdapter {
+
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void resize (int width, int height) {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+
+	@Override
+	public void pause () {
+
+	}
+
+	@Override
+	public void resume () {
+
+	}
+
+	@Override
+	public void dispose () {
+
+	}
+}
